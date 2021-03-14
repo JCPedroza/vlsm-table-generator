706564fedbfb9bd38eaad638ac16b4e83777786d
JCPedroza <nemesis4go10@hotmail.com>
2021-03-14 05:07:54 -0600

    Added some basic validation functions.

---
+++ scripts/initialize.js
@@ -0,0 +1,8 @@
+/**
+ * Creating namespaces for module encapsulation.
+ */
+(() => {
+  'use strict';
+
+  window.vlsm = {};
+})();
---
+++ scripts/validation.js
@@ -0,0 +1,27 @@
+(() => {
+  'use strict';
+
+  const isPositiveInt = (str) => {
+    const regExp = /^\+?(0|[1-9]\d*)$/;
+    return regExp.test(str);
+  };
+
+  const isIPv4 = (str) => {
+    // Tests 010.010.010.010 as valid
+    const regExp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return regExp.test(str);
+  };
+
+  const isIPv4Prefix = (str) => {
+    const hasSlash = str[0] === '/';
+    const num = parseInt(str.slice(1));
+    const inRange = num >= 8 && num <= 31;
+    return hasSlash && inRange;
+  };
+
+  window.vlsm.validation = {
+    isPositiveInt,
+    isIPv4,
+    isIPv4Prefix
+  };
+})();
